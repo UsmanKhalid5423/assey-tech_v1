11db58684f02dc0085cb0e870d4bb19326adc6ef
Usman Khalid <usman.khalid@veroke.com>
2022-03-17 23:20:25 +0500

    n

--- app/routes/general.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-const generalController = require('../controllers/general')
-
-router.post('/contactus',generalController.contactUs)
-router.get('/subscribe',generalController.subscribe)
-
-module.exports = router;
